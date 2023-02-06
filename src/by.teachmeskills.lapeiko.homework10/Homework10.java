9209e513b0950404065e811d8d854dee3b452a50
Damir Lapeiko <lapeiko02@maik.ru>
2023-02-06 23:07:35 +0300

    Homework

--- src/by.teachmeskills.lapeiko.homework10/Homework10.java
+++ src/by.teachmeskills.lapeiko.homework10/Homework10.java
@@ -1,4 +1,49 @@
 package by.teachmeskills.lapeiko.homework10;
 
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
 public class Homework10 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("What task do you want to see? Enter a number from 1 to 6: ");
+        int taskNumber = sc.nextInt();
+        Scanner sc1 = new Scanner(System.in);
+        switch (taskNumber) {
+            case 1 -> {
+                System.out.print("Create a new string: ");
+                String str = sc1.nextLine();
+                System.out.println(StringUtils.normalizeString(str));
+            }
+            case 2 -> {
+                System.out.print("Write your card numbers: ");
+                String str = sc1.nextLine();
+                System.out.println(StringUtils.getSecureCardNumber(str));
+            }
+            case 3 -> {
+                System.out.println("Lapeiko Damir");
+                System.out.println(StringUtils.initialsOfName("Lapeiko", "Damir",
+                        ""));
+            }
+            case 4 -> {
+                System.out.print("Write your passport number: ");
+                String str = sc1.nextLine();
+                System.out.println("Is your passport number belarusian?");
+                System.out.println(StringUtils.isRightNumberOfBelPassport(str));
+            }
+            case 5 -> {
+                System.out.print("Write your password: ");
+                String str = sc1.nextLine();
+                System.out.println("Is your password reliable?");
+                System.out.println(StringUtils.isReliablePassword(str));
+            }
+            case 6 -> {
+                System.out.println("Write your email");
+                String str = sc1.nextLine();
+                System.out.println("Is this line similar to email?");
+                System.out.println(StringUtils.isSimilarStringToEmail(str));
+            }
+            default -> System.out.println("Wrong number of task");
+        }
+    }
 }
--- src/by.teachmeskills.lapeiko.homework10/StringUtils.java
+++ src/by.teachmeskills.lapeiko.homework10/StringUtils.java
@@ -1,6 +1,6 @@
 package by.teachmeskills.lapeiko.homework10;
 
-public class Utils {
+public class StringUtils {
     /* Task1
      Пользователи часто в полях ввода оставляют случайные лишние пробелы.
      Создать глобальную (т.е. static) функцию, которая нормализует строку.
@@ -8,8 +8,15 @@ public class Utils {
      – в строке нет повторяющихся подряд 2 и более символов пробела
      Примеры: “ Hello world ” ➡ “Hello world” “Oleg Grigorijan” ➡ “Oleg Grigorijan” “ “ ➡ “”
      */
-    public static String normalizeString() {
-
+    public static String normalizeString(String str) {
+        if (str.length() == 0) {
+            throw new IllegalArgumentException("Error. Inadmissible size of line. Try again.");
+        }
+        while (str.contains("  ")) {
+            String replace = str.replace("  ", " ");
+            str = replace;
+        }
+        return str.trim();
     }
 
     /* Task2
@@ -17,8 +24,19 @@ public class Utils {
     приводит к строке формата “**** **** **** NNNN”, где NNNN – последние 4 цифры номера
     Пример: 1122334455667788 ➡ “**** ** ** 7788”
      */
-    public static String bankCard() {
-
+    public static String getSecureCardNumber(String str) {
+        if (str.length() != 16) {
+            throw new IllegalArgumentException("Error. Inadmissible size of card number. Try again.");
+        }
+        char[] chars = str.toCharArray();
+        for (int j = 0; j < chars.length; j++) {
+            chars[j] = '*';
+            if (j == 11) {
+                break;
+            }
+        }
+        str = String.valueOf(chars);
+        return str;
     }
 
     /* Task3
@@ -29,8 +47,16 @@ public class Utils {
     “Григорьян”, “Олег”, “Игоревич” ➡ “Григорьян О. И.”
     “Смит”, “Джеймс”, “” ➡ “Смит Д.”
      */
-    public static String initialsOfName() {
-
+    public static String initialsOfName(String lastName, String firstName, String patronymic) {
+        if (lastName.length() == 0 || firstName.length() == 0) {
+            throw new IllegalArgumentException("Error. Don't be anonymous. Represent yourself");
+        }
+        firstName = firstName.charAt(0) + ".";
+        if (!patronymic.equals("")) {
+            patronymic = patronymic.charAt(0) + ".";
+        }
+        String fullName = lastName + " " + firstName + patronymic;
+        return fullName;
     }
 
     /* Task4
@@ -40,8 +66,19 @@ public class Utils {
     регистра и 7 цифр Примеры: “MP1234567” ➡ Да “MP123456” ➡ Нет “PPP234567”
     ➡ Нет “ЯЯ1234567” ➡ Нет “PP123456P” ➡ Нет “mp1234567" ➡ Нет
      */
-    public static boolean isRightNumberOfBelPassport() {
-
+    public static boolean isRightNumberOfBelPassport(String str) {
+        boolean isRight = false;
+        if (str.length() == 9) {
+            if (str.charAt(0) >= 'A' && str.charAt(0) <= 'Z' &&
+                    str.charAt(1) >= 'A' && str.charAt(1) <= 'Z') {
+                for (int i = 2; i < str.length(); i++) {
+                    if (str.charAt(i) >= '0' && str.charAt(i) <= '9') {
+                        isRight = true;
+                    }
+                }
+            }
+        }
+        return isRight;
     }
 
     /* Task5
@@ -53,8 +90,21 @@ public class Utils {
     Примеры: “aaHlo2pm” ➡ Да “ouM-MLL23pm?Hlss” ➡ Да “бянтык23длШ”
     ➡ Да “dslfjdsljdslfjldsgjlgdsdsj” ➡ Нет “HHON83LD” ➡ Нет “aH3l” ➡ Нет
      */
-    public static boolean isReliablePassword() {
-
+    public static boolean isReliablePassword(String str) {
+        boolean isReliable = false;
+        if (str.length() >= 8) {
+            char[] password = str.toCharArray();
+            for (char i : password) {
+                if (Character.isLowerCase(i)) {
+                    if (Character.isUpperCase(i)) {
+                        if (Character.isDigit(i)) {
+                            isReliable = true;
+                        }
+                    }
+                }
+            }
+        }
+        return isReliable;
     }
 
     /* Task6
@@ -66,7 +116,15 @@ public class Utils {
     ➡ Да “a@a” ➡ Да “@ab” ➡ Нет “ab@” ➡ Нет “abc“ ➡ Нет “a bc@gmail.com”
     ➡ Нет “abc@abc@gmail.com” ➡ Нет
      */
-    public static boolean isSimilarStringToEmail() {
-
+    public static boolean isSimilarStringToEmail(String str) {
+        if (str.isBlank()) {
+            throw new IllegalArgumentException("Error. Don't be anonymous. Write your email.");
+        }
+        if (str.contains("@") && !str.contains(" ")) {
+            if (str.indexOf('@') == str.lastIndexOf('@')) {
+                return str.indexOf('@') < str.length() - 1 && str.indexOf('@') > 0;
+            }
+        }
+        return false;
     }
 }
